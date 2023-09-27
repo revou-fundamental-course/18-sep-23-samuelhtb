0f6578e5a91f4fce68d74845ab7617fc9100cab2
Samuel Haposan Tua Batubara <you@example.com>
2023-09-27 21:31:05 +0700

    revou project

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+showSlides();
+
+
+function ValidationForm() {
+    var name = document.getElementById("your-name").value;
+    var email = document.getElementById("email").value
+    var interest = document.getElementById("interest").value
+
+    if (name != "" && email!="" && interest !="") {
+        return true;
+    }else{
+        alert('Anda harus mengisi data dengan lengkap !');
+    }
+}
+
+const carouselSlides = document.querySelectorAll(".image");
+let currentIndex = 0;
+let interval = 3000;
+
+function nextSlide() {
+    carouselSlides[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % carouselSlides.length;
+    carouselSlides[currentIndex].classList.add('active');
+}
+
+function startSlide() {
+    setInterval(nextSlide, interval);
+}
+
+
